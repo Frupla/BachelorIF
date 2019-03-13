43e0b2bbed636916d3e54d4836696696cacd1eee
Frupla <s163920@student.dtu.dk>
2019-03-13 18:36:55 +0100

    Trying to do stuff with data

--- weTryToReadData.m
+++ weTryToReadData.m
@@ -1,4 +1,4 @@
-getDataFromLog('logType2 (159).csv');
+warning('OFF', 'MATLAB:table:ModifiedAndSavedVarnames')
 
 
 %%
@@ -9,31 +9,12 @@ end
 
 
 %%
-close all
-drawStuff(d0,0);
-drawStuff(d1,1);
-drawStuff(d2,2);
-drawStuff(d3,3);
-drawStuff(d4,4);
-drawStuff(d5,5);
-drawStuff(d6,6);
 
-%%
-close(19)
-figure(19)
-hold on
-plot(d0.RPM,d0.Lambda,'.')
-plot(d1.RPM,d1.Lambda,'.')
-plot(d2.RPM,d2.Lambda,'.')
-plot(d3.RPM,d3.Lambda,'.')
-plot(d4.RPM,d4.Lambda,'.')
-plot(d5.RPM,d5.Lambda,'.')
-plot(d6.RPM,d6.Lambda,'.')
-hold off
+drawStuff(d40,1)
 
 %%
 function drawStuff(data, n)
-    figure(n+1)
+    figure(n)
     hold on
     yyaxis right
     plot(data.RPM,data.InjectionDuration_us_,'.')
