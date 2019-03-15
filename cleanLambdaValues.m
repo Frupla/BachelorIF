7639cb70a6724ee59738182945c3590cc0117f0f
s163905 <s163905@student.dtu.dk>
2019-03-15 12:27:19 +0100

    The same, but now it also puts files in the correct folders

--- cleanLambdaValues.m
+++ cleanLambdaValues.m
@@ -1,8 +1,13 @@
 
 %Some proto code for applying removeEdgeLambdaValues to series of data
-for n = 4:327
-   oldfilename = strcat('logtype2 (',num2str(n),').csv')
+
+context = pwd;
+storageplace = strcat(context,'/Behandlet data/logType3');
+for n = 1:327
+   cd(context);
+   oldfilename = strcat('logtype2 (',num2str(n),').csv');
    data = getDataFromLog(oldfilename);
-   newfilename =  strcat('logtype3 (',num2str(n),').csv')
+   newfilename =  strcat('logtype3 (',num2str(n),').csv');
+   cd(storageplace);
    removeEdgeLambdaValues(data,newfilename);
 end
