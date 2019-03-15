ebb85fdb50282035497f06bcadc02328ec57bf8b
s163905 <s163905@student.dtu.dk>
2019-03-15 12:02:14 +0100

    Sorted out the edge lambda values for files 4 to 327

--- cleanLambdaValues.m
+++ cleanLambdaValues.m
@@ -1,6 +1,6 @@
 
 %Some proto code for applying removeEdgeLambdaValues to series of data
-for n = 4:33
+for n = 4:327
    oldfilename = strcat('logtype2 (',num2str(n),').csv')
    data = getDataFromLog(oldfilename);
    newfilename =  strcat('logtype3 (',num2str(n),').csv')
